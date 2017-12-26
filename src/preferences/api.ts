97ed06a97b98b6a6662ec4ccb0fa2e25dca617fd
Lauritz Hilsøe <mail@lauritz.me>
2017-12-26 19:35:52 +0100

    Update preference API call to new route

--- src/preferences/api.ts
+++ src/preferences/api.ts
@@ -2,7 +2,7 @@ import { client } from 'api';
 import { Preference } from './types';
 
 export const setPreferences = async (id: number, preferences: Preference[]) => {
-  const res = await client.post(`/schedules/${id}/setpreferences`, preferences);
+  const res = await client.put(`/schedules/${id}/preferences`, preferences);
 
   return res;
 };
